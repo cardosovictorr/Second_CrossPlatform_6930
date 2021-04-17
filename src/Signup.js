0284851ac0222c04c041a5e08077f211b5122071
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-04-17 16:20:32 +1000

    adding the project to GitHub

---
+++ src/Signup.js
@@ -0,0 +1,44 @@
+// 2 : Signup Screen Component:
+
+import * as React from 'react';
+import { Button, View, Text, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function SignupScreen({ navigation }) {
+
+  function showSignup() {
+    return (
+      <SafeAreaView>
+        <Text>New User Name</Text>
+        <TextInput
+          style={styles.input}
+        />
+        <Text>New Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+        />
+      </SafeAreaView>
+    )
+  }
+
+  return (
+    <>
+      {showSignup()}
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {/*
+          Using the navigation to rout to Home screen
+      */}
+
+        <Button title="Signup" onPress={() => navigation.navigate('HomeScreen')} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+})
