cd6db095cf5bac6d1f2fdf0de1fca2e315f73987
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-04-24 20:41:05 +1000

    adding the buttons working and the extra screens

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,86 @@
 // 4 : Home Screen Component:
 
-import * as React from 'react'
-import { Button, View, StyleSheet, Alert } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, StatusBar, Platform, Text, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const Separator = () => (
   <View style={styles.separator} />
 );
 
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
 export default function Home({ navigation }) {
+
+  //const { landscape } = useDeviceOrientation();
+
+  const AppButton = ({ onPress, title, size, backgroundColor }) => (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={[
+        styles.appButtonContainer,
+        backgroundColor && { backgroundColor }
+      ]}
+    >
+      <Text style={[styles.appButtonText, size === "sm" && { fontSize: 14 }]}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+
+  //() => navigation.navigate('MilkScreen')
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {/*
-          Using the navigation to rout to Login screen
-      */}
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-      <Separator />
-      <Button
-        title="Logout!"
-        onPress={() => navigation.navigate('LoginScreen')}
-      />
-    </View>
+
+    <SafeAreaView style={styles.container}>
+      <AppButton title="MILK" backgroundColor="#B3D6F0" onPress={() => navigation.navigate('MilkScreen')} />
+      <AppButton title="CROISSANT" backgroundColor="#FFEDC0" onPress={() => navigation.navigate('CroissantScreen')} />
+      <AppButton title="COFFEE" backgroundColor="#DBCAC6" onPress={() => navigation.navigate('CoffeeScreen')} />
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
+    justifyContent: 'space-evenly',
+    alignItems: "center",
+    //paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    //backgroundColor: "purple",
+    // marginHorizontal: 16,
   },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
+
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    height: 150,
+    width: 300
   },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
+
+  appButtonText: {
+    fontSize: 18,
+    color: "#585858",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
+  // title: {
+  //   textAlign: 'center',
+  //   marginVertical: 8,
+  // },
+  // fixToText: {
+  //   flexDirection: 'row',
+  //   justifyContent: 'space-between',
+  // },
+  // separator: {
+  //   marginVertical: 8,
+  //   borderBottomColor: '#737373',
+  //   borderBottomWidth: StyleSheet.hairlineWidth,
+  // },
 });
---
+++ src/Milk.js
@@ -0,0 +1,15 @@
+// 5 : Milk Screen Component:
+
+import * as React from 'react'
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
+
+
+export default function Milk({ navigation }) {
+    return (
+        <SafeAreaView>
+            <Text>MILK</Text>
+
+        </SafeAreaView>
+
+    );
+}
